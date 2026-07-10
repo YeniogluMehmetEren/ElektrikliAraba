26be534eb85bd68159cdfaae36a41cb04165a1d3
Mehmet Eren Yenioğlu <yenioglumehmeteren@gmail.com>
2026-07-10 12:59:56 +0300

    Lift Hareketi ve Işınlanılabilir Zemin

---
+++ Assets/Scripts/LiftMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LiftMovement : MonoBehaviour
+{
+    public Transform transformLift;
+    public float liftingSpeed;
+    public Button btnUp;
+    public Button btnDown;
+
+    Vector3 liftMin = new Vector3(0f, 0.085f, 0f);
+    Vector3 liftMax = new Vector3(0f, 4.3f, 0f);
+
+    public bool basiliTutuluyor = false;
+
+
+    public void ButonaBasildi()
+    {
+        basiliTutuluyor = true;
+    }
+
+   
+    public void ButonBirakildi()
+    {
+        basiliTutuluyor = false;
+    }
+
+    void Update()
+    {
+        if (basiliTutuluyor && btnUp.IsPressed() && transformLift.transform.position.y < liftMax.y)
+        {
+            transformLift.transform.position += Vector3.up * liftingSpeed;
+        }
+        if (basiliTutuluyor && btnDown.IsPressed() && transformLift.transform.position.y > liftMin.y )
+        {
+            transformLift.transform.position += Vector3.down * liftingSpeed;
+        }
+    }
+}
