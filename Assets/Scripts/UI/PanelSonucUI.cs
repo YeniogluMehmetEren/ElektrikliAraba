be029ff364f8e8a45fe4d3cca2b3ddf7140986fa
Mehmet Eren Yenioğlu <yenioglumehmeteren@gmail.com>
2026-08-05 17:37:18 +0300

    Sonuç ekranı tasarım bitti
     Yapılması gerenler var

---
+++ Assets/Scripts/UI/PanelSonucUI.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PanelSonucUI : MonoBehaviour
+{
+    public TextMeshProUGUI IDgun1;
+    public TextMeshProUGUI IDgun2;
+    public TextMeshProUGUI IDgun3;
+    public TextMeshProUGUI Sicaklikgun1;
+    public TextMeshProUGUI Sicaklikgun2;
+    public TextMeshProUGUI Sicaklikgun3;
+
+    public int hangiID = 1;
+
+    private List<BatteryCellUI> Gun1Degerleri;
+    private List<BatteryCellUI> Gun2Degerleri;
+    private List<BatteryCellUI> Gun3Degerleri;
+
+    void Start()
+    {
+        UIBataryaVeriGirisi uIBataryaVeriGirisi = FindAnyObjectByType<UIBataryaVeriGirisi>();
+        Gun1Degerleri = uIBataryaVeriGirisi.batteryCellDataGun1;
+        Gun2Degerleri = uIBataryaVeriGirisi.batteryCellDataGun2;
+        Gun3Degerleri = uIBataryaVeriGirisi.batteryCellDataGun3;
+        IDyeGoreSatirDoldur(hangiID); //Baþladýðý gibi ilk id 1i doldurmuyodu direkt id 1i oldursun diye 
+    }
+
+    public void SonrakiIDyeGeç()
+    {
+        HangiIDArttir();
+        IDyeGoreSatirDoldur(hangiID);
+    }
+
+    public void IDyeGoreSatirDoldur(int ID)
+    {
+        foreach (var item in Gun1Degerleri)
+        {
+            if (item.hucreID == hangiID)
+            {
+                IDgun1.text = item.idText.text;
+                Sicaklikgun1.text = item.tempText.text;
+            }
+        }
+        foreach (var item in Gun2Degerleri)
+        {
+            if (item.hucreID == hangiID)
+            {
+                IDgun2.text = item.idText.text;
+                Sicaklikgun2.text = item.tempText.text;
+            }
+        }
+        foreach (var item in Gun3Degerleri)
+        {
+            if (item.hucreID == hangiID)
+            {
+                IDgun3.text = item.idText.text;
+                Sicaklikgun3.text = item.tempText.text;
+            }
+        }
+    }
+
+    public void HangiIDArttir() { hangiID++; }
+
+
+    public void BtnNormalBasildi()
+    {
+        //Durmunu kaydeden kod yazýlýcak kafam çok karýþýk
+        SonrakiIDyeGeç();
+    }
+    public void BtnUyariBasildi()
+    {
+
+        SonrakiIDyeGeç();
+    }
+    public void BtnKritikBasildi()
+    {
+        
+        SonrakiIDyeGeç();
+    }
+}
--- Assets/Scripts/UI/UIBatteryCell.cs
+++ Assets/Scripts/UI/UIBatteryCell.cs
@@ -16,13 +16,14 @@ public class BatteryCellUI : MonoBehaviour
     public Color kritikRenk = Color.red;
     public Color olculmediRenk = Color.gray;
 
-    private int hucreID;
+    public int hucreID;
+    public string hucreDurum;
 
     public int GetHucreID() {  return hucreID; }
     public void Setup(int id)
     {
         hucreID = id;
-        idText.text = $"#{id:D2}"; // #01, #02 þeklinde gösterir
+        idText.text = $"{id:D2}";
         tempText.text = "Bilinmiyor °C";
         hucreArkaPlan.color = olculmediRenk;
     }
